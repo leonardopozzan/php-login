8edba3e299cb6bd515f9fad5cc7af4ff2dc769be
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-27 20:37:14 +0100

    add graphic  fix

--- pages/subscription.php
+++ pages/subscription.php
@@ -2,12 +2,12 @@
 include_once __DIR__ . '/../personal-db/conncection.php';
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 session_start();
 $registered = false;
 $first_name = $last_name = $email = $password = "";
-if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['password'])){
+if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['password'])) {
     echo 'sono entrato nel primo if';
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -15,9 +15,8 @@ if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['em
     $password = $_POST['password'];
     echo $first_name . " " . $last_name . " " .  $email  . " " .  $password;
     $sqlquery = "INSERT INTO users(first_name,last_name,email,password) VALUES('$first_name', '$last_name', '$email','$password')";
-    $conn->query($sqlquery);
+    $saved = $conn->query($sqlquery);
     $registered = true;
-    
 }
 
 
@@ -27,16 +26,16 @@ include_once __DIR__ . '/../partials/head.php';
 
 <body>
     <div class="container mt-5">
-        <?php 
-        if($registered){
-            if ($conn->query($sqlquery) === TRUE) {
+        <?php
+        if ($registered) {
+            if ($saved) {
                 echo "record inserted successfully";
             } else {
                 echo "Error: " . $sqlquery . "<br>" . $conn->error;
             }
         }
         ?>
-    
+
         <form action="subscription.php" method="post">
             <label for="first_name">Nome</label>
             <input type="text" name="first_name" required>
---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar  my-navbar">
+        <div class="container d-flex">
+
+            <a class="navbar-brand " href="#">
+                <img class="logo-img" src="./img/logo_site_ablp-removebg-preview.png" class="d-inline-block align-top"
+                    alt="logo page">
+                <h1 class="text-yellow d-inline ms-3">
+                    AB & LP
+                </h1>
+            </a>
+        </div>
+    </nav>
+</header>
